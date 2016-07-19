81f35631f3f3755c263ffe5985134bf5b0278afc
Carl Peterken <carboes@gmail.com>
2016-07-19 22:08:18 +1200

    updated unit tests for logger

--- test/toybot.js
+++ test/toybot.js
@@ -1,56 +1,66 @@
 import assert from 'assert'
 import ToyBot from '../lib/toybot.js'
 
+var latestMockLog = ''
+var mockConsole = {
+    log: (str) => {
+        latestMockLog = str
+    },
+    output: () => {
+        return latestMockLog
+    }
+}
+
 describe('ToyBot tests', () => {
 
-	it('Rotate left', done => {
-		var toyBot = new ToyBot('1,2,EAST')
-		toyBot.left()
-		assert.equal(toyBot.toString(),'1,2,NORTH')
-		done()
-	})
+    it('Rotate left', done => {
+        var toyBot = new ToyBot('1,2,EAST')
+        toyBot.left()
+        assert.equal(toyBot.toString(),'1,2,NORTH')
+        done()
+    })
 
-	it('Rotate right', done => {
-		var toyBot = new ToyBot('1,2,EAST')
-		toyBot.right()
-		assert.equal(toyBot.toString(),'1,2,SOUTH')
-		done()
-	})
+    it('Rotate right', done => {
+        var toyBot = new ToyBot('1,2,EAST')
+        toyBot.right()
+        assert.equal(toyBot.toString(),'1,2,SOUTH')
+        done()
+    })
 
-	it('Move', done => {
-		var toyBot = new ToyBot('1,2,EAST')
-		toyBot.move()
-		assert.equal(toyBot.toString(),'2,2,EAST')
-		done()
-	})
+    it('Move', done => {
+        var toyBot = new ToyBot('1,2,EAST')
+        toyBot.move()
+        assert.equal(toyBot.toString(),'2,2,EAST')
+        done()
+    })
 
-	it('Max bound north', done => {
-		var toyBot = new ToyBot('5,5,NORTH')
-		toyBot.move()
-		assert.equal(toyBot.toString(),'5,5,NORTH')
-		done()
-	})
+    it('Max bound north', done => {
+        var toyBot = new ToyBot('5,5,NORTH')
+        toyBot.move()
+        assert.equal(toyBot.toString(),'5,5,NORTH')
+        done()
+    })
 
-	it('Min bound south', done => {
-		var toyBot = new ToyBot('5,0,SOUTH')
-		toyBot.move()
-		assert.equal(toyBot.toString(),'5,0,SOUTH')
-		done()
-	})
+    it('Min bound south', done => {
+        var toyBot = new ToyBot('5,0,SOUTH')
+        toyBot.move()
+        assert.equal(toyBot.toString(),'5,0,SOUTH')
+        done()
+    })
 
-	it('Max bound east', done => {
-		var toyBot = new ToyBot('5,5,EAST')
-		toyBot.move()
-		assert.equal(toyBot.toString(),'5,5,EAST')
-		done()
-	})
+    it('Max bound east', done => {
+        var toyBot = new ToyBot('5,5,EAST')
+        toyBot.move()
+        assert.equal(toyBot.toString(),'5,5,EAST')
+        done()
+    })
 
-	it('Min bound west', done => {
-		var toyBot = new ToyBot('0,5,WEST')
-		toyBot.move()
-		assert.equal(toyBot.toString(),'0,5,WEST')
-		done()
-	})
+    it('Min bound west', done => {
+        var toyBot = new ToyBot('0,5,WEST')
+        toyBot.move()
+        assert.equal(toyBot.toString(),'0,5,WEST')
+        done()
+    })
 
 
     it('First command not placed', done => {
@@ -95,27 +105,30 @@ describe('ToyBot tests', () => {
         done()
     })
 
-    it('Integration a', done => {
-        var toyBot = new ToyBot('0,0,NORTH')
-        toyBot.move()
-        assert.equal(toyBot.toString(),'0,1,NORTH')
+    it('Integration a - MOVE', done => {
+        var toyBot = ToyBot.processCommand('PLACE 0,0,NORTH', null)
+        toyBot = ToyBot.processCommand('MOVE', toyBot, mockConsole)
+        toyBot = ToyBot.processCommand('REPORT', toyBot, mockConsole)
+        assert.equal(mockConsole.output(),'0,1,NORTH')
         done()
     })
 
-    it('Integration b', done => {
-        var toyBot = new ToyBot('0,0,NORTH')
-        toyBot.left()
-        assert.equal(toyBot.toString(),'0,0,WEST')
+    it('Integration b - LEFT', done => {
+        var toyBot = ToyBot.processCommand('PLACE 0,0,NORTH', null)
+        toyBot = ToyBot.processCommand('LEFT', toyBot, mockConsole)
+        toyBot = ToyBot.processCommand('REPORT', toyBot, mockConsole)
+        assert.equal(mockConsole.output(),'0,0,WEST')
         done()
     })
 
-    it('Integration c', done => {
-        var toyBot = new ToyBot('1,2,EAST')
-        toyBot.move()
-        toyBot.move()
-        toyBot.left()
-        toyBot.move()
-        assert.equal(toyBot.toString(),'3,3,NORTH')
+    it('Integration c - MOVE & LEFT', done => {
+        var toyBot = ToyBot.processCommand('PLACE 1,2,EAST', null)
+        toyBot = ToyBot.processCommand('MOVE', toyBot, mockConsole)
+        toyBot = ToyBot.processCommand('MOVE', toyBot, mockConsole)
+        toyBot = ToyBot.processCommand('LEFT', toyBot, mockConsole)
+        toyBot = ToyBot.processCommand('MOVE', toyBot, mockConsole)
+        toyBot = ToyBot.processCommand('REPORT', toyBot, mockConsole)
+        assert.equal(mockConsole.output(),'3,3,NORTH')
         done()
     })
 
